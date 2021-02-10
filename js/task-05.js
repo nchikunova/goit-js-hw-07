d6da85b508565c758ab317a65ae572cf46628130
Nataliya Chikunova <nchikunova86@gmail.com>
2021-02-10 19:48:14 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,15 @@ const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('input', event => {
-  if (event.target.value === '') {
-    outputRef.textContent = 'незнакомец';
-  } else {
-    outputRef.textContent = event.target.value;
-  }
+  event.target.value === ''
+    ? (outputRef.textContent = 'незнакомец')
+    : (outputRef.textContent = event.target.value);
 });
+
+// inputRef.addEventListener('input', event => {
+//   if (event.target.value === '') {
+//     outputRef.textContent = 'незнакомец';
+//   } else {
+//     outputRef.textContent = event.target.value;
+//   }
+// });
